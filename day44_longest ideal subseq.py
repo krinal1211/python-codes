97c4d8f7a4f35c85e8cf4cfaa68e933ab5389001
krinal1211 <161677545+krinal1211@users.noreply.github.com>
2024-04-25 20:02:15 +0530

    Update day44_longest ideal subseq.py

--- day44_longest ideal subseq.py
+++ day44_longest ideal subseq.py
@@ -1,17 +1,13 @@
 class Solution(object):
     def longestIdealString(self, s, k):
-        n = len(s)
-        dp = [[0] * 26 for _ in range(n)]
+        dp = {}  
+        for i in range(len(s)):
+            max_length = 0
+            for j in range(ord(s[i]) - k, ord(s[i]) + k + 1):
+                if chr(j) in dp:
+                    max_length = max(max_length, dp[chr(j)])
+            dp[s[i]] = max_length + 1
 
-        for i in range(n):
-            for j in range(26):
-                if i == 0:
-                    dp[i][j] = 1 if ord(s[i]) - ord('a') == j else 0
-                else:
-                    diff = abs(ord(s[i]) - ord('a') - j)
-                    if diff <= k:
-                        dp[i][j] = max(dp[i-1][j], dp[i-1][j-diff]) + 1
-                    else:
-                        dp[i][j] = dp[i-1][j]
+        return max(dp.values())
 
-        return max(max(row) for row in dp)
+
